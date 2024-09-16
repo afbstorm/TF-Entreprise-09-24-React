8a11950c9c935c1d8606dbd9cf4dfd2e207ddde0
afbstorm <aurelien.freyling@bstorm.be>
2024-09-16 16:53:19 +0200

    ajout de la démo des formulaires

--- Demo React/src/App.jsx
+++ Demo React/src/App.jsx
@@ -1,4 +1,5 @@
 import Todo from './components/Todo/todo.jsx';
+import {nanoid} from "nanoid";
 import './App.css'
 import {useState} from "react";
 
@@ -12,30 +13,97 @@ function App() {
       id: 1,
       title: 'Apprendre React',
       description: 'Les joies des useState, useEffect, etc...',
+      priority: 'high',
       isCompleted: false
     },
     {
       id: 2,
       title: 'Aller manger',
       description: 'Un bon sandwish de chez Lolotte',
+      priority: 'low',
       isCompleted: true
     },
     {
       id: 3,
       title: 'Voir les useState',
       description: 'Les états locaux, on aime',
+      priority: 'medium',
       isCompleted: true
     },
     {
       id: 4,
       title: 'Voir les formulaires',
-      description: 'Comment on rajoute un élément depuis un formulaie',
-      isCompleted: false
+      description: 'Comment on rajoute un élément depuis un formulaire',
+      priority: 'high',
+      isCompleted: true
     },
   ]);
 
+  const [newTask, setNewTask] = useState(
+      {
+        id: nanoid(),
+        title: '',
+        description: '',
+        priority: '',
+        isCompleted: false
+      }
+  )
+
+  const handleChanges = (e) => {
+    // const {name, value} = e.target;
+    // setNewTask({...newTask, [name]: value})
+    setNewTask({...newTask, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      if (newTask.id && newTask.description) {
+        setTodoList([...todoList, newTask])
+      }
+
+      setNewTask(
+          {
+            id: nanoid(),
+            title: '',
+            description: '',
+            priority: '',
+            isCompleted: false
+          }
+      )
+  }
+
   return (
-    <Todo todoList={todoList} />
+      <>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">
+            <input
+                type="text"
+                name="title"
+                value={newTask.title}
+                onChange={(e) => handleChanges(e)} />
+          </label>
+          <label htmlFor="description">
+            <textarea
+                name="description"
+                value={newTask.description}
+                onChange={(e) => handleChanges(e)}></textarea>
+          </label>
+          <label htmlFor="priority">
+            <select
+                name="priority"
+                value={newTask.priority}
+                onChange={(e) => handleChanges(e)}>
+              <option value="" disabled>Selectionner une priorité</option>
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+            </select>
+          </label>
+          <button type='submit'>Confirmer</button>
+        </form>
+        <Todo todoList={todoList} />
+      </>
+
   )
 }
 
