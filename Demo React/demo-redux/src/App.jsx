26b682b70821fb2e682cc077fa9485f1b0fa927a
Aurelien St <aurelien.strimelle@bstorm.be>
2024-09-23 15:54:14 +0200

    Demo Redux - Starter 🏁

---
+++ Demo React/demo-redux/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <span>Demo Redux</span>
+      </header>
+      <main>
+        
+      </main>
+    </>
+  )
+}
+
+export default App
